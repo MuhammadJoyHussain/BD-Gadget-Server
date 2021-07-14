4a6a561f5d52371fc5cf0de260f6ddb48b262281
unknown <muhammadjoyhussain@gmail.comdtrujhtyjnjjtdjhnghj>
2021-07-15 00:45:09 +0600

    update

--- index.js
+++ index.js
@@ -16,13 +16,11 @@ app.use(cors());
 app.use(express.json());
 
 //DB Config
-const db = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b9eao.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const db = process.env.MONGO_URI;
 
 //Connect to mongos
 mongoose.
     connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('connected'))
-    .catch(err => console.log(err))
 
 
 app.use('/api/home', home);
